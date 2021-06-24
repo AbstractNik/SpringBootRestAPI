f69eeee3c9161f50ed5d3787ff2f2a24c335f5b9
NikCoder <nikhiltyagi2000@gmail.com>
2021-06-24 22:56:19 +0100

    Added i18n to SBRESTAPI

--- src/main/java/com/sbrest/demo/controller/HelloWorldController.java
+++ src/main/java/com/sbrest/demo/controller/HelloWorldController.java
@@ -1,6 +1,12 @@
 package com.sbrest.demo.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +16,8 @@ import com.sbrest.demo.beans.UserDetails;
 @RestController
 public class HelloWorldController {
 	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	//@RequestMapping(method=RequestMethod.GET,path="/helloworld")
 	@GetMapping("/helloworld")
 	public String helloWorld() {
@@ -22,6 +30,14 @@ public class HelloWorldController {
 		
 		return new UserDetails("Nikhil", "Tyagi", "Delhi");
 		
+	}
+	@GetMapping("/hello-int")
+	public String getMessageI18n(@RequestHeader(name="Accept-Language",required=false) String locale) {
+		return messageSource.getMessage("label.hello",null, new Locale(locale));
+	}
+	@GetMapping("/hello-int2")
+	public String getMessageI18n1() {
+		return messageSource.getMessage("label.hello",null, LocaleContextHolder.getLocale());
 	}
 
 }
